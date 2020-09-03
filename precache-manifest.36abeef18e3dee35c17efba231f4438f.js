f37868a1b3b31f82fa53dbf81654ba67ddaa4e14
ASUS <anahita_fakhravar@yahoo.com>
2020-09-04 00:31:29 +0430

    Updates

---
+++ precache-manifest.36abeef18e3dee35c17efba231f4438f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7778bc42a7144a4e707509b1310607d3",
+    "url": "/FirstReactApp/index.html"
+  },
+  {
+    "revision": "3a584d42252f32060ba1",
+    "url": "/FirstReactApp/static/css/main.f86b572e.chunk.css"
+  },
+  {
+    "revision": "7f0c919c5a059d892bf8",
+    "url": "/FirstReactApp/static/js/2.8e3c9775.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FirstReactApp/static/js/2.8e3c9775.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a584d42252f32060ba1",
+    "url": "/FirstReactApp/static/js/main.a30e2a80.chunk.js"
+  },
+  {
+    "revision": "48ee3732e7b5ad0c8f48",
+    "url": "/FirstReactApp/static/js/runtime-main.28e95b04.js"
+  }
+]);
